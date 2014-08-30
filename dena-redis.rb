a73a2e6c82f4d2efa87003245f3018933ab78a95
haruyama-makoto <makoto.haruyama@dena.com>
2014-08-31 00:53:56 +0900

    Add dena-redis

---
+++ dena-redis.rb
@@ -0,0 +1,77 @@
+require 'formula'
+
+class DenaRedis < Formula
+  homepage 'https://github.com/DeNA/redis'
+  url "https://github.com/DeNA/redis.git", :branch => '2.6-dena'
+  version '2.6'
+  #bottle do
+  #  sha1 "40c0a0f119515a521825ca59641e6bb150994c90" => :mavericks
+  #  sha1 "153050a39b5072c047022beccf0d737897c7e1f9" => :mountain_lion
+  #  sha1 "cf64b6b6baeb0b2dabbf9b5684f008cb1c503056" => :lion
+  #end
+
+  #head 'https://github.com/antirez/redis.git', :branch => 'unstable'
+
+  fails_with :llvm do
+    build 2334
+    cause 'Fails with "reference out of range from _linenoise"'
+  end
+
+  def install
+    # Architecture isn't detected correctly on 32bit Snow Leopard without help
+    ENV["OBJARCH"] = "-arch #{MacOS.preferred_arch}"
+
+    # Head and stable have different code layouts
+    src = (buildpath/'src/Makefile').exist? ? buildpath/'src' : buildpath
+    system "make", "-C", src, "CC=#{ENV.cc}"
+
+    %w[benchmark cli server check-dump check-aof sentinel].each { |p| bin.install src/"redis-#{p}" }
+    %w[run db/redis log].each { |p| (var+p).mkpath }
+
+    # Fix up default conf file to match our paths
+    inreplace "redis.conf" do |s|
+      s.gsub! "/var/run/redis.pid", "#{var}/run/redis.pid"
+      s.gsub! "dir ./", "dir #{var}/db/redis/"
+      s.gsub! "\# bind 127.0.0.1", "bind 127.0.0.1"
+    end
+
+    etc.install 'redis.conf'
+    etc.install 'sentinel.conf' => 'redis-sentinel.conf'
+  end
+
+  plist_options :manual => "redis-server #{HOMEBREW_PREFIX}/etc/redis.conf"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/redis-server</string>
+          <string>#{etc}/redis.conf</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>WorkingDirectory</key>
+        <string>#{var}</string>
+        <key>StandardErrorPath</key>
+        <string>#{var}/log/redis.log</string>
+        <key>StandardOutPath</key>
+        <string>#{var}/log/redis.log</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system "#{bin}/redis-server", "--test-memory", "2"
+  end
+end
