4d24396a873e5e2fe3f56401da0b8e8d1f3f34c2
haruyama-makoto <haruyama.makoto@dena.jp>
2016-07-15 00:00:34 +0900

    Add version

--- cipherscan.rb
+++ cipherscan.rb
@@ -3,6 +3,7 @@ require 'formula'
 class Cipherscan < Formula
   homepage 'https://github.com/jvehent/cipherscan'
   url 'https://github.com/jvehent/cipherscan.git', branch: 'master'
+  version '1.0'
 
   depends_on 'bash'
   depends_on 'coreutils'
